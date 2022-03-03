71b1a7e170572b4083f44d9e3fb110cb207d800a
AKhil <akhilprm999@gmail.com>
2022-03-04 01:24:42 +0530

    macros styled

--- macros/Klass_macro.rkt
+++ macros/Klass_macro.rkt
@@ -1,80 +1,82 @@
 #lang racket
+
+;; =================================================================================================== 
+
 (require (for-syntax racket/match))
 (require (for-syntax racket/syntax))
 (require racket/stxparam)
 
 ;; interface: (Klass name (a b ...) ((func_name arg ...) func_body ...) ... )
+;; Creating a class macro.
 (define-syntax (Klass stx)
   (syntax-case stx ()
-               [(_ name (a b ...) ((func_name arg ...) func_body ...) ...)
-                (with-syntax ([obj_def (format-id #'name "define-obj-~a" #'name)])
-                #'(begin
-                    (struct name (a b ...))
-            
-                ;; creating an object of a specific class by expanding into defining a struct and defining macros for   dot operations on fields.
-                (define-syntax (obj_def stx)
-                  (syntax-case stx ()
-                               [(_ obj (val (... ...)))
-                                #`(begin
-                                    (define obj (name val (... ...))) ; available at run time.
+    [(_ name (a b ...) ((func_name arg ...) func_body ...) ...)
+     (with-syntax ([obj-def (format-id #'name "define-obj-~a" #'name)])
+       #'(begin
+           (struct name (a b ...))
 
-                                    ;; calling define-obj-field macro for all the fields in Klass.
-                                    #,@(for/list ([prop (syntax->list #'(a b ...))])
-                                                 (with-syntax ([prop-name prop]) 
-                                                 #`(define-obj-field obj name prop-name))) ; macros defined at compile time. Should be Available.
-                                    
-                                    
-                                    ;; creating an obj.func_name definition for each function in the pattern.
-                                    #,@(for/list ([func (syntax->list #'(func_name ...))]
-                                                  [args (syntax->list #'(arg ... ...))]
-                                                  [bodys (syntax->list #'(func_body ... ...))])
-                                                 (with-syntax ([obj.func_name (format-id #'obj "~a.~a" #'obj func)]
-                                                               [obj.arg  args]
-                                                               [obj.body bodys])
-                                                 #`(define (obj.func_name obj.arg)
-                                                     (syntax-parameterize (#,@(for/list ([value (syntax->list #'(a b ...))])
-                                                                                       (with-syntax  ([obj.val (format-id #'obj "~a.~a" #'obj value)] [r_val (format-id #'obj "~a" value)])
-                                                                                        #`[r_val (make-rename-transformer #'obj.val)])))
-                                                                          obj.body  
-                                                                          )))
-                                                 ))]))))]))
+           ;; creating an object of a specific class by expanding into defining a struct and defining
+           ;; macros for dot operations on fields.
+           (define-syntax (obj-def stx)
+             (syntax-case stx ()
+               [(_ obj (val (... ...)))
+                #`(begin
+                    (define obj (name val (... ...))) ; available at run time.
+
+                    ;; calling define-obj-field macro for all the fields in Klass.
+                    #,@(for/list ([prop (syntax->list #'(a b ...))])
+                         (with-syntax ([prop-name prop]) 
+                           #`(define-obj-field obj name prop-name)))
+
+                    ;; creating an obj.func_name definition for each function in the pattern.
+                    #,@(for/list ([func (syntax->list #'(func_name ...))]
+                                  [args (syntax->list #'(arg ... ...))]
+                                  [bodys (syntax->list #'(func_body ... ...))])
+                         (with-syntax ([obj.func_name (format-id #'obj "~a.~a" #'obj func)]
+                                       [obj.arg  args]
+                                       [obj.body bodys])
+                           #`(define (obj.func_name obj.arg)
+                               (syntax-parameterize (#,@(for/list ([value (syntax->list #'(a b ...))])
+                                                          (with-syntax  ([obj.val (format-id #'obj
+                                                                                             "~a.~a"
+                                                                                             #'obj
+                                                                                             value)]
+                                                                         [r_val (format-id #'obj
+                                                                                           "~a"
+                                                                                           value)])
+                                                            #`[r_val (make-rename-transformer #'obj.val)])))
+                                                    obj.body)))))]))))]))
 
 
 ;; creating a macro for obj.field.
 (define-syntax (define-obj-field stx)
   (syntax-case stx ()
-               [(_ obj class field)
-                (with-syntax ([obj.field (format-id #'obj "~a.~a" #'obj #'field)]
-                              [class-field (format-id #'obj "~a-~a" #'class #'field)]
-                              )
-                             #'(define-syntax-rule (obj.field)
-                                                   (class-field obj))
-                                 #;(define-syntax-parameter self.field 
-                                 (lambda (stx1)
-                                   (raise-syntax-error (syntax-e stx1) "cannot be used outside of object definition")))
-                                  )]))
+    [(_ obj class field)
+     (with-syntax ([obj.field (format-id #'obj "~a.~a" #'obj #'field)]
+                   [class-field (format-id #'obj "~a-~a" #'class #'field)])
+       #'(define-syntax-rule (obj.field)
+           (class-field obj)))]))
 
-
-#;(define-syntax one 1)
 
 ;; A macro to initialize the syntax-parameters for all the fields in a Klass.
 (define-syntax (Klass-skeleton stx)
   (syntax-case stx ()
-               [(_ field ...)
-                #`(begin
-                    #,@(for/list ([val (syntax->list #'(field ...))])
-                                 (with-syntax ([name val]
-                                               )
-                                              #'(define-syntax-parameter name 
-                                                                                     #;(make-rename-transformer #'one)
-                                 (lambda (stx1)
-                                   (raise-syntax-error (syntax-e stx1) "cannot be used outside of object definition"))))))]))
+    [(_ field ...)
+     #`(begin
+         #,@(for/list ([val (syntax->list #'(field ...))])
+              (with-syntax ([name val])
+                #'(define-syntax-parameter name (lambda (stx1) 
+                                                  (raise-syntax-error (syntax-e stx1)
+                                                                      "cannot be used outside of def"))))))]))
 
+
+;; =================================================================================================== 
+
+;; Run
 (Klass-skeleton name no)
-(Klass bike (name no) ((get-no x) (display (+ x (no)))) ((get-name x) (display (name))))
+(Klass bike (name no) ((get-no x) (displayln (+ x (no)))) ((get-name x) (displayln (name))))
 (define-obj-bike first ("hayabusa" 10))
 (first.name)
 (first.get-no 2)
 (first.get-name 3)
-
 
--- macros/my_macros.rkt
+++ macros/my_macros.rkt
@@ -1,4 +1,7 @@
 #lang racket
+
+;; =================================================================================================== 
+
 (require (for-syntax racket/match))
 (require (for-syntax racket/syntax))
 (require macro-debugger/stepper)
@@ -11,57 +14,51 @@
 ;; second case: (name-a/b ... inst) ->  (list-ref lst no)
 (define-syntax (my-struct stx)
   (syntax-case stx ()
-               [(my-struct name (a b ...)) (syntax (define-syntax (name stx1)
-                                                              (syntax-case stx1 ()
-                                                                           [(name x y (... ...)) (syntax (list 'name x y (... ...)))])))]))
+    [(my-struct name (a b ...))
+     (syntax (define-syntax (name stx1)
+               (syntax-case stx1 ()
+                 [(name x y (... ...)) (syntax (list 'name x y (... ...)))])))]))
 
-;;(my-struct first (id sid))
-#;(let ([x 1] [y 2])
-     (first x y)
-     )
 
 ;; the second case wasn't possible with the path I was on. 
 ;; the correct way is to define three functions instead of a macro.
 (define-syntax (our-struct stx)
   (syntax-case stx ()
-               [(_ id (fields ...)) 
-                (with-syntax ([pred-id (format-id #'id "~a?" #'id)])
-                    #`(begin
-                        (define (id fields ...)
-                          (apply vector (cons 'id (list fields ...))))
-                        ;; Define the predicate function.
-                        (define (pred-id v)
-                          (and (vector? v)
-                            (eq? (vector-ref v 0) 'id)))
-                        ;; Define the accessor function for each field.
-                        #,@(for/list ([x (syntax->list #'(fields ...))]
-                                   [n (in-naturals 1)])
-                                   (with-syntax ([acc-id (format-id #'id "~a-~a" #'id x)]
-                                                 [ix n])
-                                                #`(define (acc-id v)
-                                                    (unless (pred-id v)
-                                                      (error 'acc-id "~a is not a ~a struct" v 'id))
-                                                    (vector-ref v ix))))))]))
-;;(expand/step-text (our-struct second (id sid)))
-#;(let ([x 1] [y 2])
-     (second x y)
-     )
+    [(_ id (fields ...)) 
+     (with-syntax ([pred-id (format-id #'id "~a?" #'id)])
+       #`(begin
+           (define (id fields ...)
+             (apply vector (cons 'id (list fields ...))))
+           ;; Define the predicate function.
+           (define (pred-id v)
+             (and (vector? v)
+                  (eq? (vector-ref v 0) 'id)))
+           ;; Define the accessor function for each field.
+           #,@(for/list ([x (syntax->list #'(fields ...))]
+                         [n (in-naturals 1)])
+                (with-syntax ([acc-id (format-id #'id "~a-~a" #'id x)]
+                              [ix n])
+                  #`(define (acc-id v)
+                      (unless (pred-id v)
+                        (error 'acc-id "~a is not a ~a struct" v 'id))
+                      (vector-ref v ix))))))]))
 
 
 ;; creating a macro for for/list.
-;; (for/list ([var lst]) body0 body ...) -> (define a function with params as (list)  that accumulates in a list and call it as well.
+;; (for/list ([var lst]) body0 body ...) -> (define a function with params as (list)
+;; that accumulates in a list and call it as well.
 ;; example: (for/list ([var '(1 2 3)]) (+ var 1))
 (define-syntax (my-for/list stx)
   (syntax-case stx ()
-               [(_ ([var lst]) body0 body ...)
-                #'(begin
-                    (define (for lt)
-                      (cond [(empty? lt) '()]
-                            [else (let* ([var (first lt)]
-                                         [result (begin body0 body ...)])
-                                       (cons result (for (rest lt))))]))
-                    ;; Function call.
-                    (for lst))]))
+    [(_ ([var lst]) body0 body ...)
+     #'(begin
+         (define (for lt)
+           (cond [(empty? lt) '()]
+                 [else (let* ([var (first lt)]
+                              [result (begin body0 body ...)])
+                         (cons result (for (rest lt))))]))
+         ;; Function call.
+         (for lst))]))
 
 ;; verification.
 (my-for/list ([v '(1 2 3)]) (display "hey") (+ v 1))
@@ -69,17 +66,17 @@
 
 (define-syntax (my-for stx)
   (syntax-case stx ()
-               [(_ func ([var lst]) body0 body ...)
-                   #'(begin
-                       (define (for lt)
-                          (cond [(empty? lt) (void)]
-                                [else (let* ([var (first lt)]
-                                             [result (begin body0 body ...)])
-                                           (func result (for (rest lt))))]))
-                        ;; Function call.
-                        (for lst))]))
+    [(_ func ([var lst]) body0 body ...)
+     #'(begin
+         (define (for lt)
+           (cond [(empty? lt) (void)]
+                 [else (let* ([var (first lt)]
+                              [result (begin body0 body ...)])
+                         (func result (for (rest lt))))]))
+         ;; Function call.
+         (for lst))]))
 
-
+;; run.
 (my-for and ([v '(1 2 x)]) (display "hey") (number? v))
 
 
@@ -87,11 +84,12 @@
 ;; interface: (while test body ...)
 (define-syntax (while stx)
   (syntax-case stx ()
-               [(while test body ...)
-                #'(let loop () (if test
-                                   (begin body ... (loop))
-                                   (display "done")))]))
+    [(while test body ...)
+     #'(let loop () (if test
+                      (begin body ... (loop))
+                      (display "done")))]))
 
+;; run
 (let ([i 0])
-  (while (< i 5) (displayln i) (set! i (+ i 1)))) ; kind of seems useless in the sense that the outer let should have been redundant if we used the inner named let purposefully.
+  (while (< i 5) (displayln i) (set! i (+ i 1)))) 
 
